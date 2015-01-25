106a8d2d517aee7fae02f4ea2c9456e213d77d0e
Tejas Mehta <tejasmehta0@gmail.com>
2015-01-25 18:04:35 -0500

    add performacne

--- mishift/dashboard/admin.py
+++ mishift/dashboard/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from models import UserProfile
+from models import UserProfile, PreviousTransfers, Event
 
 __author__ = 'tmehta'
 
 
 admin.site.register(UserProfile)
+admin.site.register(Event)
+admin.site.register(PreviousTransfers)
